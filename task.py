ed241935cc04ec8591ce67129d129bc34ec87981
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-30 21:58:35 +0530

    added sol to Q6

--- task.py
+++ task.py
@@ -61,6 +61,24 @@ col=sliced[:,-1]
 print(np.dot(row,col))
 
 
+#Q6 DICTIONARIES
+
+cutoffs=[("Pilani","CS",327),("Pilani","Chemical",247),("Pilani","M.Sc. Bio",236,),
+("Pilani","M.Sc. Eco",271),
+
+("Goa","CS",301),("Goa","Chemical",239),("Goa","M.Sc. Bio",234),("Goa","M.Sc. Eco",263),
+
+("Hyderabad","CS",298),("Hyderabad","Chemical",238),("Hyderabad","M.Sc. Bio",234),
+("Hyderabad","M.Sc. Eco",261)]
+
+college_dict={}
+for college,branch,marks in cutoffs:
+    if college not in college_dict:
+        college_dict[college]={}
+    college_dict[college][branch]=marks
+
+print(college_dict)
+
 
 #Q7 INTERSECTION OF TWO ARRAYS
 
