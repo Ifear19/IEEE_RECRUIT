f0eab5657ee1f83267801afe705be189d4b27508
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-30 23:37:14 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,9 +0,0 @@
-#Q4 SLICING
-
-import numpy as np
-matrix=np.random.randint(1,101,size=(5,5))
-sliced=matrix[1:4,1:4]#indexing starts from 0 and last number in slicing is not included
-print("\n sliced matrix\n",sliced)
-row=sliced[0,:]#taking the first row
-col=sliced[:,-1]#taking the first col
-print("\n DOT PRODUCT\n",np.dot(row,col))
