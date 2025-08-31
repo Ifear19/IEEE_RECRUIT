3d2e291d80c97fc1a0894ba3c287aa52dbd4b745
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-31 18:51:09 +0530

    Update Q7.py

--- IEEE_RECRUIT_TASKS/Q7.py
+++ IEEE_RECRUIT_TASKS/Q7.py
@@ -3,12 +3,13 @@
 list1=[3, 4, 5, 1, 4, 6, 1, 7, 7]
 list2=[5, 8, 2, 9, 9, 4, 6, 3]
 intersect=[]
-for i in list1:
-    if i in list2 and i not in intersect: #this will prevent the repetition of numbers
-        intersect.append(i)
+for num in list1:
+    if num in list2 and num not in intersect: #this will prevent the repetition of numbers
+        intersect.append(num)
 if len(intersect)==0:
     print("THERE ARE NO COMMON NUMBERS")
 else:
     print("\nlist of common numbers is \n",intersect)  
+
 
 
