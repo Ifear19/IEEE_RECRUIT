5f95c82a2c2820b2b49afc2b042c061537d5b983
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-31 18:40:46 +0530

    Update Q3.py

--- IEEE_RECRUIT_TASKS/Q3.py
+++ IEEE_RECRUIT_TASKS/Q3.py
@@ -14,7 +14,8 @@ print("mean value:",np.mean(matrix))
 normalised=((matrix-np.min(matrix))/(np.max(matrix)-np.min(matrix)))
 print("\nnormalised matrix\n" ,normalised.round(2) ) #used the round fun to make the matrix look more clean
 
-flat_sort=np.sort(matrix.flatten())
+flat_sort=np.sort(matrix.flatten())#this will first flatten the matrix and then sort it
 
 print("\nflattened and sorted array\n",flat_sort)#using \n sequence to get ouput in other line
+
 
