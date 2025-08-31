9894ee06341cb931553c7171b34057e80daf8233
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-31 19:12:37 +0530

    Update Q1.py

--- IEEE_RECRUIT_TASKS/Q1.py
+++ IEEE_RECRUIT_TASKS/Q1.py
@@ -2,6 +2,7 @@
 
 n=int(input('Enter a Number for the star pattern '))
 for i in range(1,n+1): #range fun doesn't include the last value so it will make a star of n lines only
-     print(' '*(n-i),'*'*i)#by using this statement I don't need an inner loop
+     print(' '*(n-i),'*'*i)#by using this statement I don't need to use nested loop
 
    
+
