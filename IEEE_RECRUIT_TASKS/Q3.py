b2e58cc7e6027f49b30e5ca8005d44b56fce7bd4
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-31 18:46:51 +0530

    Update Q3.py

--- IEEE_RECRUIT_TASKS/Q3.py
+++ IEEE_RECRUIT_TASKS/Q3.py
@@ -11,11 +11,13 @@ print("maximum value:",np.max(matrix))
 print("minimum value:",np.min(matrix))
 print("mean value:",np.mean(matrix))
 
-normalised=((matrix-np.min(matrix))/(np.max(matrix)-np.min(matrix)))
+normalised=((matrix-np.min(matrix))/(np.max(matrix)-np.min(matrix)))#this operation will make the smallest value 0
+#and largest 1 and all other reamining would be in between 0 an 1
 print("\nnormalised matrix\n" ,normalised.round(2) ) #used the round fun to make the matrix look more clean
 
 flat_sort=np.sort(matrix.flatten())#this will first flatten the matrix and then sort it
 
 print("\nflattened and sorted array\n",flat_sort)#using \n sequence to get ouput in other line
+
 
 
