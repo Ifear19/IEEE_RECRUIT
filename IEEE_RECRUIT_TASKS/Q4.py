349882932ac12d718a03ef55773700920f140872
saksham gupta <f20250473@pilani.bits-pilani.ac.in>
2025-08-31 11:57:26 +0530

    Update Q4.py

--- IEEE_RECRUIT_TASKS/Q4.py
+++ IEEE_RECRUIT_TASKS/Q4.py
@@ -5,5 +5,6 @@ matrix=np.random.randint(1,101,size=(5,5))
 sliced=matrix[1:4,1:4]#indexing starts from 0 and last number in slicing is not included
 print("\n sliced matrix\n",sliced)
 row=sliced[0,:]#taking the first row
-col=sliced[:,-1]#taking the first col
+col=sliced[:,-1]#taking the last col
+
 print("\n DOT PRODUCT\n",np.dot(row,col))
